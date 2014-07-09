f5e8a086de9a778cc1457e32a063ab18f43ece26
Harish Rajagopal <ApigeeCorporation@Harish-Rajagopals-MacBook-Pro.local>
2014-07-09 15:31:07 -0700

    adding files for tests on import

--- stack/services/src/main/java/org/apache/usergrid/management/importUG/S3Import.java
+++ stack/services/src/main/java/org/apache/usergrid/management/importUG/S3Import.java
@@ -1,7 +1,13 @@
 package org.apache.usergrid.management.importUG;
 
+import java.io.File;
+import java.util.Map;
+
 /**
  * Created by ApigeeCorporation on 7/8/14.
  */
 public interface S3Import {
+    File copyFromS3(Map<String,Object> exportInfo, String filename );
+
+    String getFilename ();
 }
