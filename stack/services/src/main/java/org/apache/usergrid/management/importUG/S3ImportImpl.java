6628b2197a480f95c3090e81cce37d399da9250f
Pooja Jain <ApigeeCorporation@Pooja-Jains-MacBook-Pro.local>
2014-07-09 15:34:30 -0700

    Revert "added implementation for copyFromS3 and doImport"
    This reverts commit a6437da73a8eba756262ce0531adb9b104dece94.

--- stack/services/src/main/java/org/apache/usergrid/management/importUG/S3ImportImpl.java
+++ stack/services/src/main/java/org/apache/usergrid/management/importUG/S3ImportImpl.java
@@ -1,19 +1,7 @@
 package org.apache.usergrid.management.importUG;
 
-import java.io.File;
-import java.util.Map;
-
 /**
  * Created by ApigeeCorporation on 7/8/14.
  */
-public class S3ImportImpl implements S3Import {
-    @Override
-    public File copyFromS3(Map<String, Object> importInfo, String filename) {
-        return new File("ephemeral");
-    }
-
-    @Override
-    public String getFilename() {
-        return null;
-    }
+public class S3ImportImpl {
 }
